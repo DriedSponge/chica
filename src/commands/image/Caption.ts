83d9b621e66c065a56d101424beb0cbf29f4687d
jordan <jordan@driedsponge.net>
2023-07-27 13:07:09 -0700

    Got cpation command working on gifs

--- src/commands/image/Caption.ts
+++ src/commands/image/Caption.ts
@@ -1,10 +1,9 @@
 import {SlashCommand} from "../SlashCommand";
-import {GifFrame, GifUtil, GifCodec, Gif, BitmapImage} from "gifwrap";
-import axios from 'axios';
-import {Canvas, CanvasRenderingContext2D, createCanvas, Image, loadImage, registerFont} from "canvas";
+import {GifUtil, GifCodec, Gif, BitmapImage, GifFrame} from "gifwrap";
+import {Canvas, CanvasRenderingContext2D, createCanvas, Image, loadImage} from "canvas";
 import {
-    Attachment, ChatInputCommandInteraction,
-    CommandInteraction, EmbedBuilder,
+    ChatInputCommandInteraction,
+    EmbedBuilder,
     SlashCommandAttachmentOption,
     SlashCommandBuilder,
     SlashCommandStringOption
@@ -29,73 +28,86 @@ export class Caption extends SlashCommand{
             await interaction.deferReply();
             const editImage : EditImage = await CommandUtils.processImageCommand(interaction);
             const image: Image = await loadImage(editImage.getData());
-            const fontSize : number = Math.min(image.width,image.height) / 9;
+
+            const fontSize : number = Math.min(image.width,image.height) / 10;
+
             const tempCanvas: Canvas = createCanvas(image.width, image.height);
             const tempCtx : CanvasRenderingContext2D = tempCanvas.getContext("2d");
             tempCtx.font = `bold ${fontSize.toString()}px Caption`;
-            const lines : string[] = StringUtils.wrapText(tempCtx, text, tempCanvas.width);
-            const textHeight: number = (tempCtx.measureText(lines[0]).actualBoundingBoxAscent * (lines.length+1))+20;
+            const lines : string[] = StringUtils.wrapText(tempCtx, text, tempCanvas.width - (tempCanvas.width/9));
+
+            const yMargin :number = (tempCtx.measureText(lines[0]).actualBoundingBoxAscent)*1.7
+
+            const textHeight: number = ((tempCtx.measureText(lines[0]).actualBoundingBoxAscent) * (lines.length+1))+(yMargin);
             const canvas: Canvas = createCanvas(image.width, image.height+textHeight);
 
             const ctx : CanvasRenderingContext2D = canvas.getContext("2d");
             ctx.fillStyle = "#ffffff";
             ctx.textAlign = "center";
             ctx.textBaseline = "top";
-            ctx.font = `bold ${fontSize.toString()}px Caption`;
+            ctx.font = `s${fontSize.toString()}px Caption`;
             ctx.fillRect(0,0,canvas.width,textHeight)
             ctx.fillStyle = "#000000";
-
             // Check if the image is static
             if(editImage.isStatic()){
                 ctx.drawImage(image, 0, textHeight, canvas.width, image.height);
-                this.captionText(canvas, lines, fontSize);
+                this.captionText(canvas, lines, fontSize,yMargin);
                 const buffer : Buffer = canvas.toBuffer();
-                await interaction.followUp({files: [{ attachment: buffer, name:"meme."+editImage.getExtension()}]});
+                await interaction.followUp({files: [{ attachment: buffer, name:"caption."+editImage.getExtension()}]});
             }else{
                 // If the image is a gif, we need to process each frame
                 let gifBuffer : Buffer = editImage.getData();
                 const gif: Gif = await GifUtil.read(gifBuffer);
-
+                let i :number = 0;
+                // We need to create a new collection of frames and a whole new gif because to modify the existing one (like the meme command), the gf
+                // needs to stay the same size.
+                const outputFrames : GifFrame[] = [];
                 for (const frame of gif.frames) {
-                    console.log("Processing Gift Frame");
+                    i++
+                    console.log(`[${i}/${gif.frames.length}] Processing Gif Frame`);
                     // Create a canvas for the gif frame
-                    const gifCanvas: Canvas = createCanvas(canvas.width,  image.height);
+                    const gifCanvas: Canvas = createCanvas(canvas.width,  canvas.height);
                     // Get the context of the canvas
                     const gifCtx: CanvasRenderingContext2D = gifCanvas.getContext("2d");
-                    // Get the image data from the frame
+                    // Get the image data buffer from the current frame
                     const frameImage: Buffer = frame.bitmap.data;
-                    // Create an image data object from the frame
+                    // Create an image data object from the buffer we just created
                     const frameImageData: ImageData = <ImageData>ctx.createImageData(frame.bitmap.width, frame.bitmap.height);
                     // Set the image data to the frame image
                     frameImageData.data.set(frameImage);
                     // Put the image data on the gif canvas
-                    gifCtx.putImageData(frameImageData, frame.xOffset, frame.yOffset);
+                    gifCtx.putImageData(frameImageData, frame.xOffset, frame.yOffset+textHeight);
                     // Clear the main canvas
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
                     // Draw the gif frame on the main canvas
-                    ctx.drawImage(gifCanvas, frame.xOffset+textHeight, frame.yOffset, frame.bitmap.width, frame.bitmap.height)
+                    ctx.drawImage(gifCanvas, 0, 0)
+
+                    ctx.fillStyle = "#ffffff";
+                    ctx.fillRect(0,0,canvas.width,textHeight)
+                    ctx.fillStyle = "#000000";
+
                     // Put the text on the main canvas
-                    this.captionText(canvas, lines, fontSize);
+                    this.captionText(canvas, lines, fontSize,yMargin);
                     // Get the image data from the main canvas
-                    const modifiedFrameImage: ImageData = <ImageData>ctx.getImageData(frame.xOffset+textHeight,frame.yOffset, frame.bitmap.width, frame.bitmap.height);
+                    const modifiedFrameImage: ImageData = <ImageData>ctx.getImageData(0,0, canvas.width, canvas.height);
                     // Create a buffer for the image data
                     const modifiedFrameBuffer: Buffer = Buffer.alloc(modifiedFrameImage.data.length);
                     // Set the buffer to the image data
                     modifiedFrameBuffer.set(modifiedFrameImage.data);
                     // Create a bitmap image from the buffer
-                    const frameBitmap: BitmapImage = new BitmapImage(frame.bitmap.width, frame.bitmap.height, modifiedFrameBuffer);
+                    const frameBitmap: BitmapImage = new BitmapImage(canvas.width, canvas.height, modifiedFrameBuffer);
                     // Quantize the bitmap image
                     GifUtil.quantizeSorokin(frameBitmap, 256,undefined,{ditherAlgorithm:"FalseFloydSteinberg"}) // This is the fastest
                     // Set the frame bitmap to the quantized bitmap
-                    frame.bitmap.data.set(frameBitmap.bitmap.data);
+                    outputFrames.push(new GifFrame(frameBitmap.bitmap))
                 }
                 // Encode the gif
                 const codec: GifCodec = new GifCodec();
-                const gifBuffer2: Gif = await codec.encodeGif(gif.frames, {loops: gif.loops});
+                const gifBuffer2: Gif = await codec.encodeGif(outputFrames, {loops: gif.loops});
                 const embed: EmbedBuilder  = new EmbedBuilder();
-                embed.setImage("attachment://meme."+editImage.getExtension());
+                embed.setImage("attachment://caption."+editImage.getExtension());
                 embed.setFooter({text: `${image.width}x${image.height}  | Frames: ${gif.frames.length} | Time Taken: ${(Date.now() - interaction.createdTimestamp)/1000}s`})
-                await interaction.followUp({files: [{ attachment: gifBuffer2.buffer, name:"meme."+editImage.getExtension()}], embeds: [embed]});
+                await interaction.followUp({files: [{ attachment: gifBuffer2.buffer, name:"caption."+editImage.getExtension()}], embeds: [embed]});
             }
         }catch (e) {
             console.log(e)
@@ -103,10 +115,10 @@ export class Caption extends SlashCommand{
         }
 
     }
-    private captionText(canvas : Canvas, lines : string[], fontSize : number): void{
+    private captionText(canvas : Canvas, lines : string[], fontSize : number, yMargin: number): void{
         let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
         for(let i: number = 0; i < lines.length; i++) {
-            ctx.fillText(lines[i], canvas.width / 2, (i * fontSize) + 10, canvas.width);
+            ctx.fillText(lines[i], canvas.width / 2, (i * fontSize) + (yMargin/2), canvas.width);
         }
     }
 }
